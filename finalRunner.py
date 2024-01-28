9bb938ea1bb963d5ad3745ba58c3e0037b7865af
Shaqiloheal <w.spalding@toc@gmail.com>
2024-01-28 12:16:13 +0000

    Better enemy spawn logic (with timers)

--- finalRunner.py
+++ finalRunner.py
@@ -1,6 +1,7 @@
 # Import necessary libraries
 import pygame
 from sys import exit
+from random import randint
 
 
 def display_score():
@@ -9,6 +10,31 @@ def display_score():
     score_rect = score_surface.get_rect(center=(400, 50))
     screen.blit(score_surface, score_rect)
     return current_time
+
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            if obstacle_rect.bottom == 300:
+                screen.blit(snail_surface, obstacle_rect)
+            else:
+                screen.blit(fly_surface, obstacle_rect)
+
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+
+        return obstacle_list
+    else:
+        return []
+
+
+def collisions(player, obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect):
+                return False
+    return True
 
 
 # Initialise Pygame
@@ -29,13 +55,12 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-# Create text surface for score and position it
-# score_surface = test_font.render('My Game', False, (64, 64, 64))
-# score_rect = score_surface.get_rect(center=(400, 50))
-
+# Obstacles
 # Load snail image, enable transparency, and set its initial position
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surface.get_rect(bottomright=(600, 300))
+fly_surface = pygame.image.load('graphics/Fly/Fly1.png').convert_alpha()
+
+obstacle_rect_list = []
 
 # Load player image, enable transparency, and set initial position and gravity
 player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
@@ -56,6 +81,10 @@ game_message_rect = game_message.get_rect(center=(400, 320))
 game_over_message = test_font.render(f'GAME OVER!', False, (111, 196, 169))
 game_over_rect = game_over_message.get_rect(center=(400, 320))
 
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
+
 # Main game loop
 while True:
     for event in pygame.event.get():
@@ -75,9 +104,14 @@ while True:
                     player_gravity = -20
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                snail_rect.left = 800
                 start_time = int(pygame.time.get_ticks() / 1000)
                 game_active = True
+
+        if event.type == obstacle_timer and game_active:
+            if randint(0, 2):
+                obstacle_rect_list.append(snail_surface.get_rect(bottomright=(randint(900, 1100), 300)))
+            else:
+                obstacle_rect_list.append(fly_surface.get_rect(bottomright=(randint(900, 1100), 210)))
 
     if game_active:
 
@@ -93,10 +127,10 @@ while True:
 
         # Snail
         # Move the snail and reset its position if it goes off-screen
-        snail_rect.x -= 4
-        if snail_rect.right <= 0:
-            snail_rect.left = 800
-        screen.blit(snail_surface, snail_rect)
+        # snail_rect.x -= 4
+        # if snail_rect.right <= 0:
+        #     snail_rect.left = 800
+        # screen.blit(snail_surface, snail_rect)
 
         # Player
         # Apply gravity to the player and ensure they land on the ground
@@ -106,12 +140,17 @@ while True:
             player_rect.bottom = 300
         screen.blit(player_surface, player_rect)
 
+        # Obstacle movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
         # Collision
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+        game_active = collisions(player_rect, obstacle_rect_list)
+
     else:
         screen.fill((94, 129, 162))
         screen.blit(player_stand, player_stand_rect)
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80, 300)
 
         score_message = test_font.render(f'Score: {score}', False, (111, 196, 169))
         score_message_rect = score_message.get_rect(center=(400, 360))
